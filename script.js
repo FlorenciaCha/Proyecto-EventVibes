69818c142ca383c34b08c3614c1857dbb7a0e791
FlorenciaCha <163207162+FlorenciaCha@users.noreply.github.com>
2024-05-26 17:46:41 -0300

    Proyecto EventVibes
    Se subieron los códigos html, css, javascript e imágenes del proyectoEventVibes

---
+++ script.js
@@ -0,0 +1,10 @@
+ /* Agrega funcionalidad al boton contacto para que redirija a la pagina contacto*/
+ document.getElementById('contacto-btn').addEventListener('click', function() {
+    window.location.href = './contacto.html';
+});
+
+
+ /* Agrega funcionalidad al boton Ver Mas en la seccion de eventos para redirigir a la pagina de eventos*/
+ document.getElementById('btn-ver-eventos').addEventListener('click', function() {
+    window.location.href = './eventos.html';
+});
